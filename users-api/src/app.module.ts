696e30be06224c1e37def0833745517574e6e70d
Stefan Stevic <stefan@steviss.dev>
2024-11-27 11:48:52 +0100

    chore: add nestjs config and configuration file, to alias process envs

--- users-api/src/app.module.ts
+++ users-api/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ users-api/src/config/configuration.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  tables: {
+    users: process.env.USERS_TABLE,
+  },
+});
